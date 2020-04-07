fea0977b3e4069b667131ddfab8f353d6ce8f62b
daiti0113 <stennis.ds.0113@gmail.com>
2020-04-07 22:01:46 +0900

    fix:Updated RefineMenu style

--- src/components/RefineItem.js
+++ src/components/RefineItem.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
@@ -18,14 +17,11 @@ const useStyles = makeStyles({
     paddingTop: 0,
     paddingBottom: 0,
   },
-  icon: {
-    minWidth: 32,
-  }
 });
 
 export default function RefineItem(props) {
   const classes = useStyles();
-  const {input, title, type, icon} = props;
+  const {input, title, type} = props;
   const [nestOpen, setNestOpen] = useState(false);
   
   const handleNestOpen = () => {
@@ -48,11 +44,8 @@ export default function RefineItem(props) {
   return (
     <React.Fragment>
       <ListItem className={classes.refineRabel} onClick={handleNestOpen}>
-        <ListItemIcon className={classes.icon}>
-          {icon}
-        </ListItemIcon>
         <ListItemText primary={title} />
-        {nestOpen ? <ExpandLess fontSize="large" /> : <ExpandMore fontSize="large" />}
+        {nestOpen ? <ExpandLess /> : <ExpandMore />}
       </ListItem>
       <Collapse in={nestOpen} timeout="auto" unmountOnExit>
         {inputItem[input]}
--- src/components/RefineMenu.js
+++ src/components/RefineMenu.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import LocalAtmIcon from '@material-ui/icons/LocalAtm';
+import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
@@ -22,33 +22,45 @@ const useStyles = makeStyles({
 
 export default function RefineMenu() {
   const classes = useStyles();
-  const [refineNestOpen, setRefineNestOpen] = useState(true);
+  const [open, setOpen] = useState(true);
+  const [refineDetailOpen, setRefineDetailOpen] = useState(false);
   
-  function handleRefineNestOpen() {
-    setRefineNestOpen(!refineNestOpen);
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const handleRefineDetailOpen = () => {
+    setRefineDetailOpen(!refineDetailOpen);
   };
 
   return (
     <React.Fragment>
-      <ListItem button key="title-refine" onClick={handleRefineNestOpen}>
+      <ListItem button key="title-refine" onClick={handleOpen}>
         <Typography variant="h6" className={classes.title}>
           絞り込み
-          {refineNestOpen ? <ExpandLess fontSize="large" /> : <ExpandMore fontSize="large" />}
+          {open ? <ExpandLess fontSize="large" /> : <ExpandMore fontSize="large" />}
         </Typography>
       </ListItem>
 
-      <Collapse in={refineNestOpen} timeout="auto" unmountOnExit>
+      <Collapse in={open} timeout="auto" unmountOnExit>
         <List component="div" disablePadding>
-          <RefineItem input="slider" title="価格" type="price" icon={<LocalAtmIcon />} />
-          <RefineItem input="checkbox" title="メーカー" type="manufacturer" icon={<LocalAtmIcon />} />
-          <RefineItem input="slider" title="長さ" type="length" icon={<LocalAtmIcon />} />
-          <RefineItem input="slider" title="太さ" type="thickness" icon={<LocalAtmIcon />} />
-          <RefineItem input="checkbox" title="本体材質" type="material" icon={<LocalAtmIcon />} />
-          <RefineItem input="checkbox" title="チップ材質" type="tipMaterial" icon={<LocalAtmIcon />} />
-          <RefineItem input="checkbox" title="チップ形状" type="tipShape" icon={<LocalAtmIcon />} />
-          <RefineItem input="checkbox" title="テーパー" type="taper" icon={<LocalAtmIcon />} />
-          <RefineItem input="slider" title="評価" type="evaluation" icon={<LocalAtmIcon />} />
-          <RefineItem input="slider" title="評価件数" type="evaluationCount" icon={<LocalAtmIcon />} />
+          <RefineItem input="slider" title="価格" type="price" />
+          <RefineItem input="checkbox" title="メーカー" type="manufacturer" />
+          <RefineItem input="slider" title="評価" type="evaluation" />
+          <RefineItem input="slider" title="評価件数" type="evaluationCount" />
+
+          <ListItem button key="title-refine" onClick={handleRefineDetailOpen}>
+            <ListItemText primary="もっと絞り込む" />
+            {refineDetailOpen ? <ExpandLess /> : <ExpandMore />}              
+          </ListItem>
+          <Collapse in={refineDetailOpen} timeout="auto" unmountOnExit>
+            <RefineItem input="slider" title="長さ" type="length" />
+            <RefineItem input="slider" title="太さ" type="thickness" />
+            <RefineItem input="checkbox" title="本体材質" type="material" />
+            <RefineItem input="checkbox" title="チップ材質" type="tipMaterial" />
+            <RefineItem input="checkbox" title="チップ形状" type="tipShape" />
+            <RefineItem input="checkbox" title="テーパー" type="taper" />
+          </Collapse>
         </List>
       </Collapse>
     </React.Fragment>
